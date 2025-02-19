79aa73984d5fc943064703bb00e7b675ad80e18c
Tristan Martinez <tmartinez@alset.com.mx>
2025-02-19 08:47:37 -0600

    upgrade 9

--- aspnet-core/TestUpgrade/Migrations/20250219142740_ABP_9.Designer.cs
+++ aspnet-core/TestUpgrade/Migrations/20250219142740_ABP_9.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TestUpgrade.Data;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace TestUpgrade.Migrations
 {
     [DbContext(typeof(TestUpgradeDbContext))]
-    partial class TestUpgradeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250219142740_ABP_9")]
+    partial class ABP_9
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ aspnet-core/TestUpgrade/Migrations/20250219142740_ABP_9.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestUpgrade.Migrations
+{
+    /// <inheritdoc />
+    public partial class ABP_9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreationTime",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorId",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropColumn(
+                name: "DeleterId",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropColumn(
+                name: "DeletionTime",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropColumn(
+                name: "LastModificationTime",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifierId",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropColumn(
+                name: "CreationTime",
+                table: "OpenIddictAuthorizations");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorId",
+                table: "OpenIddictAuthorizations");
+
+            migrationBuilder.DropColumn(
+                name: "DeleterId",
+                table: "OpenIddictAuthorizations");
+
+            migrationBuilder.DropColumn(
+                name: "DeletionTime",
+                table: "OpenIddictAuthorizations");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "OpenIddictAuthorizations");
+
+            migrationBuilder.DropColumn(
+                name: "LastModificationTime",
+                table: "OpenIddictAuthorizations");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifierId",
+                table: "OpenIddictAuthorizations");
+
+            migrationBuilder.DropColumn(
+                name: "FlagIcon",
+                table: "AbpLanguages");
+
+            migrationBuilder.CreateTable(
+                name: "AbpSessions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SessionId = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Device = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    DeviceInfo = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    TenantId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ClientId = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    IpAddresses = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: true),
+                    SignedIn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastAccessed = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ExtraProperties = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpSessions", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpSessions_Device",
+                table: "AbpSessions",
+                column: "Device");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpSessions_SessionId",
+                table: "AbpSessions",
+                column: "SessionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpSessions_TenantId_UserId",
+                table: "AbpSessions",
+                columns: new[] { "TenantId", "UserId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AbpSessions");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationTime",
+                table: "OpenIddictTokens",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatorId",
+                table: "OpenIddictTokens",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DeleterId",
+                table: "OpenIddictTokens",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletionTime",
+                table: "OpenIddictTokens",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "OpenIddictTokens",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModificationTime",
+                table: "OpenIddictTokens",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LastModifierId",
+                table: "OpenIddictTokens",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationTime",
+                table: "OpenIddictAuthorizations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatorId",
+                table: "OpenIddictAuthorizations",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DeleterId",
+                table: "OpenIddictAuthorizations",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletionTime",
+                table: "OpenIddictAuthorizations",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "OpenIddictAuthorizations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModificationTime",
+                table: "OpenIddictAuthorizations",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LastModifierId",
+                table: "OpenIddictAuthorizations",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FlagIcon",
+                table: "AbpLanguages",
+                type: "nvarchar(48)",
+                maxLength: 48,
+                nullable: true);
+        }
+    }
+}
